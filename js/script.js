e7428e92f800cba117d07032d5c5ce0292ca2961
tallesvn <tallesvinicius062@gmail.com>
2021-12-12 22:30:43 -0300

    Adição de API, jQuery e mudanças no input

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// conferir senhas
+$('#senha2').on('focusout', e => {
+    let a = $('#senha1').val()
+    let b = $('#senha2').val()
+
+    if(a != b) {
+        $('#senha2').css('border', 'solid 2px red')
+            $('#erroSenha').css('display', 'block')
+            $('#acertoSenha').css('display', 'none')
+    } else {
+        $('#senha2').css('border', 'solid 2px green')
+            $('#acertoSenha').css('display', 'block')
+            $('#erroSenha').css('display', 'none')
+    }
+})
+
+// cep
+$('#cep').on('focusout', e => {
+    var cep = $('#cep').val()
+
+    if(cep.length == 8) {
+        let url = `https://viacep.com.br/ws/${cep}/json/`
+
+        fetch(url).then(function(response) { // fetch -> PARA CONSUMIR A API | then -> ESPERA UMA FUNÇÃO o parametro RESPONSE
+            response.json().then(mostrarEndereço) // pega o response, o json e passa o THEN para converter para um objeto
+        }) 
+    } else if(cep.length == 0 | cep.length > 8) {
+        alert('Por favor digite o CEP!')
+    } else {
+        $('#cep').append('<p>correto</p>')
+    }
+})
+
+function mostrarEndereço(dados) {
+    let endereco = $('#endereco')
+    let bairro = $('#bairro')
+    let cidade = $('#cidade')
+    let uf = $('#uf')
+
+    if(dados.erro) {
+        alert('Não foi possível encontrar o CEP!')
+    } else {
+        endereco.val(dados.logradouro)
+        bairro.val(dados.bairro)
+        cidade.val(dados.localidade)
+        uf.val(dados.uf)
+    }
+}
